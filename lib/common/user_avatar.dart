8b62280b86125a6fb7e4545a61dc8a504cfe7781
yuyanjun <yuyanjun@zego.im>
2022-02-18 15:33:03 +0800

    🐛 update avatar logic

--- lib/common/user_avatar.dart
+++ lib/common/user_avatar.dart
@@ -3,8 +3,12 @@ import 'dart:convert';
 import 'package:crypto/crypto.dart';
 
 int getUserAvatarIndex(String userName) {
-  var avatarCode = int.parse(
-      md5.convert(utf8.encode(userName)).toString().substring(0, 2),
-      radix: 16);
-  return avatarCode % 8;
+  var digest = md5.convert(utf8.encode(userName));
+  var value0 = digest.bytes[0] & 0xff;
+  return (value0 % 8).abs();
+
+  // var avatarCode = int.parse(
+  //     md5.convert(utf8.encode(userName)).toString().substring(0, 2),
+  //     radix: 16);
+  // return avatarCode % 8;
 }
